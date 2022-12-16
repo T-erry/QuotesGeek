d8562eb046a165f4791b986639608957b8e46840
T-erry <terrywachuka10@gmail.com>
2022-12-16 14:14:44 +0300

    added another js file

---
+++ index.js
@@ -0,0 +1,18 @@
+let generateQuote = document.getElementById("generate-quote")
+let cardText = document.querySelector(".card-text");
+let author = document.getElementById("author");
+
+
+function randomQuote(){
+    fetch("https://type.fit/api/quotes")
+    .then((response)=> response.json())
+    .then((quotes)=> {
+        let index = Math.round(Math.random()* 1643)
+       
+      cardText.innerText = quotes[index].text;
+      author.innerText = quotes[index].author;
+       })
+    
+}
+
+generateQuote.addEventListener("click", randomQuote)
