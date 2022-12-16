895222c9bc224c9a91bce2cbef17aed5deebe372
T-erry <terrywachuka10@gmail.com>
2022-12-16 14:16:10 +0300

    refactored the code

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let quoteList = document.querySelector(".quote-list");
 let addQuoteForm = document.getElementById("add-quote-form");
-let titleValue = document.getElementById("quote-value");
+let quoteValue = document.getElementById("quote-value");
 let authorValue = document.getElementById("author-value");
 let h2 = document.getElementById("quotes-section");
+let likeBtn =document.getElementById("like-btn");
+let icon = document.getElementById("icon");
+let count = document.getElementById("count")
 let output = "";
 const url = "http://localhost:3000/quotes";
+
 
 function renderQuotes(quotes) {
   quotes.forEach((quote) => {
@@ -14,16 +18,15 @@ function renderQuotes(quotes) {
         <div class="card-body" data-id=${quote.id}>
           <h5 class="card-title">${quote.author}</h5>
           <p class="card-text">${quote.text}</p>
-          <a href="#" class="btn btn-primary id="edit-quote">Edit</a>
           <a href="#" class="btn btn-primary" id="delete-quote">Delete</a>
         </div>
       </div>
-        `;
+        `; 
+
     quoteList.innerHTML = output;
     quoteList.addEventListener("click", (e) => {
-      console.log(e.target);
+      e.preventDefault();
       let deleteButtonPressed = e.target.id === "delete-quote";
-      let EditButtonPressed = e.target.id === "edit-quote";
       let id = e.target.parentElement.dataset.id;
       // DELETE method
       if (deleteButtonPressed) {
@@ -31,16 +34,11 @@ function renderQuotes(quotes) {
           method: "DELETE",
         });
       }
-      // PATCH METHOD
-    });
+    }); 
   });
+  
 }
-// GET request
-document.addEventListener("DOMContentLoaded", () => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((quotes) => renderQuotes(quotes));
-});
+
 
 //POST-insert new post
 addQuoteForm.addEventListener("submit", (e) => {
@@ -53,8 +51,8 @@ addQuoteForm.addEventListener("submit", (e) => {
       Accept: "application/json",
     },
     body: JSON.stringify({
+      text: quoteValue.value,
       author: authorValue.value,
-      text: titleValue.value,
     }),
   })
     .then((response) => response.json())
@@ -62,5 +60,15 @@ addQuoteForm.addEventListener("submit", (e) => {
       const quotesArray = [];
       quotesArray.push(quotes);
       renderQuotes(quotes);
+      form.reset();
     });
 });
+
+// GET method
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((quotes) => renderQuotes(quotes));
+});
+ // button clicked
+ 
