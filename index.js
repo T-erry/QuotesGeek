420be5e18e5c670a21f31a52925362294f7bd69c
T-erry <terrywachuka10@gmail.com>
2022-12-19 23:01:09 +0300

    refactored external api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let generateQuote = document.getElementById("generate-quote")
 let cardText = document.querySelector(".card-text");
 let author = document.getElementById("author");
 
-
+// GET method 
 function randomQuote(){
     fetch("https://type.fit/api/quotes")
     .then((response)=> response.json())
@@ -11,8 +11,7 @@ function randomQuote(){
        
       cardText.innerText = quotes[index].text;
       author.innerText = quotes[index].author;
-       })
-    
+       });
 }
 
 generateQuote.addEventListener("click", randomQuote)
